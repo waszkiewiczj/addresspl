fe2d716f965e914470f7ce54e5a8a5f64c062732
Michał Urawski <michurawski@gmail.com>
2021-05-10 22:59:27 +0200

    Acities

--- acities.py
+++ acities.py
@@ -1,6 +1,14 @@
 from typing import List
 from models import City
+from fuzzywuzzy import fuzz
 
+def get_cities(address: str, cities: List[str])-> List[City]:
+    N_MAX = 5
+    result = []
 
-def get_cities(ad)-> List[City]:
-    return ["warszawa"]
+    for city in cities:
+        score = 1 if city in address else fuzz.token_set_ratio(address, city) 
+        result.append(City(name=city, score=score))
+    result_by_len = sorted(result, lambda city: len(city.name))
+    result_sorted = sorted(result_by_len, lambda city: city.score)
+    return result_sorted[:N_MAX]

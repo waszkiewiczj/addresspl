0787be9bdf56c87e0598a8ae3bc485ee7655d48f
Tomek Zalewski <11194646+tomzal97@users.noreply.github.com>
2021-05-10 22:40:19 +0200

    PoC getStreets

--- astreets.py
+++ astreets.py
@@ -1,2 +1,24 @@
-def get_streets(ad:str, c:str):
-    return []
+def get_streets(adress:str, streets:str):
+    N = 5 # TODO: Fix n
+    scores = get_scores(address, streets)
+
+    sorted_scores = sort_scores(scores)
+    return sorted_scores[:N]
+
+def get_scores(address, streets):
+    scores = []
+
+    for street in streets:
+        if street in address:
+            scores.append({'score': 1, 'name': street})
+            continue
+
+        r = fuzz.token_set_ratio(address, street) 
+        scores.append({'score': r, 'name': street})
+    return best_c, best_r
+
+def sort_scores(scores):
+    sortedLen = sorted(scores, key=lambda score: len(score['name']), reverse=True)
+    sortedR = sorted(sortedLen, key=lambda score: score['score'], reverse=True)
+
+    return sortedR
